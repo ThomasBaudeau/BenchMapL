b5ce545acc79ed818a1efa4f959b752ac0553a8f
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2022-10-15 18:21:36 +0200

    variant.2

--- Workflow2/script/variant_maker.py
+++ Workflow2/script/variant_maker.py
@@ -1,14 +1,44 @@
 import functools
 import operator
+import random
+
+def mutate(nuc):
+    nucleotide=['A','T','C','G']
+    mut = nucleotide[random.randint(0, 3)]
+    while mut==nuc:
+        mut = nucleotide[random.randint(0, 3)]
+    return mut 
 
 def makerandom(file,wt,input,output,param):
     seq=''.join(wt.readlines()[1:]).replace('\n','')
-    randominsert(seq,pos='random')
-    randomdel(seq,pos='random')
-    randomsub(seq,pos='random')
+    name='>'#todo
+    nbevent=random.randint(0,2)
+    if nbevent==0:   
+        seq,name=randominsert(seq, name, pos='random')
+    if nbevent == 1:
+        seq, name = randomdel(seq, name, pos='random')
+    if nbevent == 2:
+        seq, name = randomsub(seq, name, pos='random')
+    file.write(name)
+    file.write(seq)
+    file.close()
+    return 
 
-def sperandom(file,input,output,param):
-    pass
+
+def sperandom(file, wt, input, output, param):
+    seq = ''.join(wt.readlines()[1:]).replace('\n', '')
+    name='>'#todo
+    #todo pour extraire les params et faire attention au position des différentes mutations
+    if nbevent==0:   
+        seq,name=randominsert(seq, name, pos='random')
+    if nbevent == 1:
+        seq, name = randomdel(seq, name, pos='random')
+    if nbevent == 2:
+        seq, name = randomsub(seq, name, pos='random')
+    file.write(name)
+    file.write(seq)
+    file.close()
+    return 
 
 
 def do_something(data_path, out_path,param):
@@ -26,9 +56,9 @@ def do_something(data_path, out_path,param):
         wildtype=open(snakemake.input[1],'r')
         file2=open(snakemake.input[1],'a')
         if len(spevariant)<variant:
-            makerandom(file2,wildtype)
+            makerandom(file, wt, input, output, param)
         else:
-            sperandom
+            sperandom(file, wt, input, output, param)
         
     files1.close()
     files2=open(snakemake.output[0],'a')
@@ -38,9 +68,28 @@ def do_something(data_path, out_path,param):
     os.remove(snakemake.input[2]+'_0001.ref')
 
 
-ok=open('Workflow2/data/ref_vih.fasta','r')
-print()
 
-randominsert(seq,pos='random')
-randomdel(seq,pos='random')
-randomsub(seq,pos='random')
+
+def randominsert(seq,name,pos='random'):
+    if pos == 'random':
+        pos=random.randint(0,len(seq)-1)
+    mut=mutate(None)
+    seq = seq[:pos]+mut+seq[pos:]
+    name += str(pos+1)+'I_'
+    return seq,name
+
+
+def randomdel(seq, name, pos='random'):
+    if pos == 'random':
+        pos = random.randint(0, len(seq)-1)
+    seq = seq[:pos-1]+seq[pos:]
+    name += str(pos)+'D_'
+    return seq, name
+
+def randomsub(seq, name, pos='random'):
+    if pos == 'random':
+        pos = random.randint(0, len(seq)-1)
+    mut=mutate(seq[pos])
+    seq = seq[0:pos-1]+mut+seq[pos:]
+    name += str(pos)+'S_'
+    return seq,name
